895f8cee2c7eba397b0cd7b6d83fd15cd1e160e2
Natesh <natesh246@gmail.com>
2020-06-03 11:05:38 +0530

    added userlogin and welcome page

---
+++ onlinebanking/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using onlinebanking.Models;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace onlinebanking.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+   
+
+        [HttpPost]
+        public ActionResult Index(LoginClass lcObj)
+        {
+            string con = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            SqlConnection sqlCon = new SqlConnection(con);
+            string sqlQuery = "select id,user_password from CUSTOMER where email_id=@EmailId and user_password=@UserPasssword";
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand(sqlQuery,sqlCon);
+            sqlCmd.Parameters.AddWithValue("@EmailId", lcObj.email_id);
+            sqlCmd.Parameters.AddWithValue("@UserPasssword", lcObj.user_password);
+            SqlDataReader sdr = sqlCmd.ExecuteReader();
+            if (sdr.Read())
+            {
+                Session["emailid"] = lcObj.email_id.ToString();
+                return RedirectToAction("welcome");
+            }
+            else
+            {
+                ViewData["Message"] = "User Login Failed";
+            }
+            sqlCon.Close();
+
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ onlinebanking/Models/LoginClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace onlinebanking.Models
+{
+    public class LoginClass
+    {
+        [Required(ErrorMessage ="Please enter the email id")]
+        [Display(Name ="Email Id")]
+        public string email_id{ get; set; }
+
+        [Required(ErrorMessage ="Please enter the Password")]
+        [Display(Name ="Password")]
+        public string user_password { get; set; }
+    }
+}
