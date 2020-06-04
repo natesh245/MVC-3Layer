b76ff3c9a44f63fdceaab5eee6b8e2c8b1e4c73c
Natesh <natesh246@gmail.com>
2020-06-04 15:01:33 +0530

    added customer details

---
+++ onlinebanking/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using onlinebanking.Models;
+
+namespace onlinebanking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Details(int id)
+        {
+
+            Customer cusObj = new Customer();
+            string con = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            SqlConnection sqlCon = new SqlConnection(con);
+            string sqlQuery = "select * from CUSTOMER where id="+id.ToString();
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand(sqlQuery, sqlCon);
+            SqlDataReader sdr = sqlCmd.ExecuteReader();
+            if (sdr.Read())
+            {
+                cusObj.customer_name = sdr["customer_name"].ToString();
+                cusObj.fathers_name = sdr["fathers_name"].ToString();
+                cusObj.date_of_birth = sdr["date_of_birth"].ToString();
+                cusObj.customer_age = Convert.ToInt32(sdr["customer_age"]);
+                cusObj.martial_status = sdr["martial_status"].ToString();
+                cusObj.customer_address = sdr["customer_address"].ToString();
+                cusObj.customer_city= sdr["customer_city"].ToString();
+                cusObj.customer_state = sdr["customer_state"].ToString();
+                cusObj.customer_country = sdr["customer_country"].ToString();
+                cusObj.pincode= Convert.ToInt32(sdr["pincode"]);
+                cusObj.phone = Convert.ToInt64(sdr["phone"]);
+                cusObj.email_id = sdr["email_id"].ToString();
+            }
+            else
+            {
+                ViewData["Message"] = "User Login Failed";
+            }
+            sqlCon.Close();
+            return View(cusObj);
+        }
+    }
+}
---
+++ onlinebanking/Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace onlinebanking.Models
+{
+    public class Customer
+    {
+        
+        [Display(Name = "Name")]
+        public string customer_name { get; set; }
+        [Display(Name = "Fathers Name")]
+        public string fathers_name { get; set; }
+        [Display(Name = "DOB")]
+      
+        public  string date_of_birth { get; set; }
+        [Display(Name = "Age")]
+       
+        public int customer_age { get; set; }
+        [Display(Name = "Martial Status")]
+       
+        public string martial_status { get; set; }
+        [Display(Name = "Address")]
+       
+        public string customer_address { get; set; }
+        [Display(Name = "City")]
+        
+        public string customer_city { get; set; }
+        [Display(Name = "State")]
+       
+        public string customer_state { get; set; }
+        [Display(Name = "Country")]
+        public string customer_country { get; set; }
+        public int pincode { get; set; }
+        public long phone { get; set; }
+        [Display(Name = "Email Id")]
+      
+        public string email_id { get; set; }
+        [Display(Name = "User Name")]
+       
+        public string user_name { get; set; }
+    }
+}
