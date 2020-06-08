1506b8f777746acd1ae0f0145cd75ac770117b71
Natesh <natesh246@gmail.com>
2020-06-08 16:59:53 +0530

    Added statements

--- onlinebanking/Controllers/TransactionsController.cs
+++ onlinebanking/Controllers/TransactionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -41,7 +42,7 @@ namespace onlinebanking.Controllers
                 sqlCon.Close();
                 ViewData["Message"] = " Transaction Successfull";
                 return View();
-               
+
             }
             catch
             {
@@ -53,7 +54,50 @@ namespace onlinebanking.Controllers
 
 
         }
+
+        public ActionResult Statement()
+        {
+            Statements sObj = new Statements();
+            DataSet ds = new DataSet();
+
+            string con = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            SqlConnection sqlCon = new SqlConnection(con);
+
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("p_transaction_details", sqlCon);
+            sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@ACCOUNT", Session["Accountno"]);
+            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+            da.Fill(ds);
+            List<Statements> Transactionlist = new List<Statements>();
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                Statements uobj = new Statements();
+                uobj.Id = Convert.ToInt32(ds.Tables[0].Rows[i]["id"].ToString());
+                uobj.IdCredit = Convert.ToInt32(ds.Tables[0].Rows[i]["id_debit"].ToString());
+               
+                uobj.DebitAccount = Convert.ToInt64(ds.Tables[0].Rows[i]["debit_account_no"].ToString());
+                uobj.CreditAccount = Convert.ToInt64(ds.Tables[0].Rows[i]["credit_account_no"].ToString());
+                uobj.DebitAccountBalance = Convert.ToInt64(ds.Tables[0].Rows[i]["debit_account_balance"].ToString());
+                uobj.CreditAccountBalance = Convert.ToInt64(ds.Tables[0].Rows[i]["credit_account_balance"].ToString());
+                uobj.DebitAmount = Convert.ToInt32(ds.Tables[0].Rows[i]["debit_amount"].ToString());
+                uobj.CreditAmount = Convert.ToInt32(ds.Tables[0].Rows[i]["credit_amount"].ToString());
+                uobj.DebitDate = ds.Tables[0].Rows[i]["debit_date_time"].ToString();
+                uobj.CreditDate = ds.Tables[0].Rows[i]["credit_date_time"].ToString();
+
+                Transactionlist.Add(uobj);
+            }
+            sObj.statementList = Transactionlist;
+            sqlCon.Close();
+            return View(sObj);
+        }
+       
+
+    } 
+}
+
+           
+        
             
 
-    }
-}
+    
