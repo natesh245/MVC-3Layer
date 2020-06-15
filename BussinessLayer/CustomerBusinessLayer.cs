557a40647259594fdcb383ed5fd1fe0e556ee3e0
Natesh <natesh246@gmail.com>
2020-06-15 18:42:26 +0530

    Customer 3 Layer Done

--- BussinessLayer/CustomerBusinessLayer.cs
+++ BussinessLayer/CustomerBusinessLayer.cs
@@ -11,17 +11,58 @@ namespace BussinessLayer
 {
  public class CustomerBusinessLayer
     {
-        public SqlDataReader GetCustomerDetails(int ID)
+        public CustomerModel GetCustomerDetails(int ID)
         {
             CustomerDataAccess cusDL = new CustomerDataAccess();
-           return cusDL.ReadDetails(ID);
+           SqlDataReader sdr= cusDL.ReadDetails(ID);
+            CustomerModel cusModel = new CustomerModel();
+            if (sdr.Read())
+            {
+                cusModel.Name = sdr["customer_name"].ToString();
+                cusModel.FatherName = sdr["fathers_name"].ToString();
+                cusModel.DateOfBirth = sdr["date_of_birth"].ToString();
+                cusModel.Age = Convert.ToInt32(sdr["customer_age"]);
+                cusModel.MaritalStatus = sdr["martial_status"].ToString();
+                cusModel.Address = sdr["customer_address"].ToString();
+                cusModel.City = sdr["customer_city"].ToString();
+                cusModel.State = sdr["customer_state"].ToString();
+                cusModel.Country = sdr["customer_country"].ToString();
+                cusModel.Pincode = Convert.ToInt32(sdr["pincode"]);
+                cusModel.Phone = Convert.ToInt64(sdr["phone"]);
+                cusModel.EmailId = sdr["email_id"].ToString();
+
+            }
+            
+            CustomerDataAccess.sqlCon.Close();
+            return cusModel;
 
         }
 
-        public SqlDataReader DisplayEditCustomer(int Id)
+        public CustomerModel DisplayEditCustomer(int Id)
         {
             CustomerDataAccess cusDL = new CustomerDataAccess();
-            return cusDL.UpdateDetails(Id);
+            CustomerModel cusModel = new CustomerModel();
+           SqlDataReader sdr =cusDL.UpdateDetails(Id);
+            if (sdr.Read())
+            {
+
+                cusModel.Name = sdr["customer_name"].ToString();
+                cusModel.FatherName = sdr["fathers_name"].ToString();
+                cusModel.DateOfBirth = sdr["date_of_birth"].ToString();
+                cusModel.Age = Convert.ToInt32(sdr["customer_age"]);
+                cusModel.MaritalStatus = sdr["martial_status"].ToString();
+                cusModel.Address = sdr["customer_address"].ToString();
+                cusModel.City = sdr["customer_city"].ToString();
+                cusModel.State = sdr["customer_state"].ToString();
+                cusModel.Country = sdr["customer_country"].ToString();
+                cusModel.Pincode = Convert.ToInt32(sdr["pincode"]);
+                cusModel.Phone = Convert.ToInt64(sdr["phone"]);
+                cusModel.EmailId = sdr["email_id"].ToString();
+
+            }
+
+            CustomerDataAccess.sqlCon.Close();
+            return cusModel;
         }
 
         public void EditCustomerDetails(int id,string name,string fn,string dob,int age,string Ms,
@@ -30,7 +71,7 @@ namespace BussinessLayer
             CustomerDataAccess cusDl = new CustomerDataAccess();
             cusDl.EditDetails(id,name,fn,dob,age,Ms,address,city,state,country,pincode,
                 phone,emailid);
-
+            CustomerDataAccess.sqlCon.Close();
         }
     }
 }
--- onlinebanking/Controllers/CustomerController.cs
+++ onlinebanking/Controllers/CustomerController.cs
@@ -17,57 +17,20 @@ namespace onlinebanking.Controllers
         public ActionResult Details(int id)
         {
             CustomerBusinessLayer cusBl = new CustomerBusinessLayer();
-            CustomerModel cusModel = new CustomerModel();
-           SqlDataReader sdr =cusBl.GetCustomerDetails(id);
+           
+           CustomerModel cusModel=cusBl.GetCustomerDetails(id);
 
-            if (sdr.Read())
-            {
-                cusModel.Name = sdr["customer_name"].ToString();
-                cusModel.FatherName = sdr["fathers_name"].ToString();
-                cusModel.DateOfBirth = sdr["date_of_birth"].ToString();
-                cusModel.Age = Convert.ToInt32(sdr["customer_age"]);
-                cusModel.MaritalStatus = sdr["martial_status"].ToString();
-                cusModel.Address = sdr["customer_address"].ToString();
-                cusModel.City= sdr["customer_city"].ToString();
-                cusModel.State = sdr["customer_state"].ToString();
-                cusModel.Country = sdr["customer_country"].ToString();
-                cusModel.Pincode= Convert.ToInt32(sdr["pincode"]);
-                cusModel.Phone = Convert.ToInt64(sdr["phone"]);
-                cusModel.EmailId = sdr["email_id"].ToString();
-              
-            }
-            else
-            {
-                ViewData["Message"] = "User Login Failed";
-            }
-           CustomerDataAccess.sqlCon.Close();
             return View(cusModel);
         }
+
         [HttpGet]
         public ActionResult Edit(int id)
         {
             CustomerBusinessLayer cusBl = new CustomerBusinessLayer();
-            CustomerModel cusModel = new CustomerModel();
-            SqlDataReader sdr=cusBl.DisplayEditCustomer(id);
-            if (sdr.Read())
-            {
-                ViewData["CustomerMessage"] = "";
-                cusModel.Name = sdr["customer_name"].ToString();
-                cusModel.FatherName = sdr["fathers_name"].ToString();
-                cusModel.DateOfBirth = sdr["date_of_birth"].ToString();
-                cusModel.Age = Convert.ToInt32(sdr["customer_age"]);
-                cusModel.MaritalStatus = sdr["martial_status"].ToString();
-                cusModel.Address = sdr["customer_address"].ToString();
-                cusModel.City = sdr["customer_city"].ToString();
-                cusModel.State = sdr["customer_state"].ToString();
-                cusModel.Country = sdr["customer_country"].ToString();
-                cusModel.Pincode = Convert.ToInt32(sdr["pincode"]);
-                cusModel.Phone = Convert.ToInt64(sdr["phone"]);
-                cusModel.EmailId = sdr["email_id"].ToString();
-
-            }
            
-            CustomerDataAccess.sqlCon.Close();
+            CustomerModel cusModel = cusBl.DisplayEditCustomer(id);
+          
+            ViewData["CustomerMessage"] = "";
             return View(cusModel);
 
         }
@@ -82,10 +45,8 @@ namespace onlinebanking.Controllers
             cusBl.EditCustomerDetails(id,cusModel.Name,cusModel.FatherName,cusModel.DateOfBirth,
                 cusModel.Age,cusModel.MaritalStatus,cusModel.Address,cusModel.City,cusModel.State,
                 cusModel.Country,cusModel.Pincode,cusModel.Phone,cusModel.EmailId);
-      
-          
+            
             ViewData["CustomerMessage"] = "Changes Saved Successfully";
-            CustomerDataAccess.sqlCon.Close();
             return View();
 
 
